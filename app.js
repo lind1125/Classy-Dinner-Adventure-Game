52196e8026c9e99a87f04c5951ce8ccf1ab3214a
lind1125 <danieljlinden@gmail.com>
2020-09-29 20:01:18 -0500

    added click even listeners to interactive elements

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // inventory object - empty array
 let inventory = []
+//actions container
+const clickableWords = document.getElementsByClassName('interactive')
 // action buttons
 const walkTo = document.getElementById('walk-to')
 const pickUp = document.getElementById('pick-up')
 const drop = document.getElementById('drop')
 const push = document.getElementById('push')
 const talkTo = document.getElementById('talk-to')
+
+// add event listeners to all interactive text that sets the clicked action to 'active'
+const generateButtons = () => {
+    for(i=0; i<clickableWords.length; i++) {
+        clickableWords[i].addEventListener('click', makeActive)
+    }
+}
+
+const makeActive = () => {
+
+}
--- to-do.md
+++ to-do.md
@@ -3,8 +3,13 @@
 # Game requirements
 
 * User must be able to acquire an object. The object must then be added to the inventory.
+* Inventory must display acquired objects and update dynamically
 * User must be able to interact with objects, both within and without the inventory
 * Status of objects must be able to be updated, and the game must recognize these changes and behave differently based on the state of an object
+
+## Stretch
+* Display window to update action taken
+* More visually oriented inventory
 
 
 
