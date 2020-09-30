c7177d4cba0a0f476112ad7d9f41afc92473ee4e
lind1125 <danieljlinden@gmail.com>
2020-09-29 21:35:07 -0500

    added action display, modified action buttons, wrote basic mechanics of click event callback function

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // inventory object - empty array
 let inventory = []
+// action display window
+let actionUpdate = document.querySelector('#action-display')
+let displayedAction = actionUpdate.children[0]
+let displayedTarget = actionUpdate.children[1]
+
 //actions container
 const clickableWords = document.getElementsByClassName('interactive')
 // action buttons
 const walkTo = document.getElementById('walk-to')
-const pickUp = document.getElementById('pick-up')
-const drop = document.getElementById('drop')
-const push = document.getElementById('push')
+const lookAt = document.getElementById('look-at')
 const talkTo = document.getElementById('talk-to')
+const grab = document.getElementById('grab')
+const drop = document.getElementById('drop')
+const use = document.getElementById('use')
 
 // add event listeners to all interactive text that sets the clicked action to 'active'
 const generateButtons = () => {
@@ -17,9 +23,20 @@ const generateButtons = () => {
 }
 
 const makeActive = (e) => {
-    //highlight selected text and add it to the display window
-    const newClass = e.target.classList
-    newClass.toggle('selected')
+    //highlight selected text 
+    e.target.classList.toggle('selected')
+    // add it to the display window
+    let displayText = e.target.innerText
+    if (e.target.classList.contains('selected')){
+        if (e.target.classList.contains('action')) {
+            displayedAction.innerText = displayText
+        }else {
+            displayedTarget.innerText = displayText
+        }
+    }else {
+        displayedAction.innerText = ''
+        displayedTarget.innerText = ''
+    }  
 }
 
 
--- to-do.md
+++ to-do.md
@@ -6,9 +6,9 @@
 * Inventory must display acquired objects and update dynamically
 * User must be able to interact with objects, both within and without the inventory
 * Status of objects must be able to be updated, and the game must recognize these changes and behave differently based on the state of an object
+* Display window to update action taken
 
 ## Stretch
-* Display window to update action taken
 * More visually oriented inventory
 
 
