914fd3136b5d90d776b33b08cc8c00a9aee21472
lind1125 <danieljlinden@gmail.com>
2020-09-29 23:43:12 -0500

    built out inventory item class; built template for action results pop-up

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
-// inventory object - empty array
-let inventory = []
+
+// inventory items
+class Item {
+    constructor (name, inHand=false){
+        this.name = name;
+        this.inHand = inHand;
+    }
+    walkTo(){
+        // default modal "Don't be silly. You can't do that."
+        let modal = document.querySelector('#result');
+        let span = document.querySelector('.close');
+        let content = document.querySelector('.modal-content').children[0]
+        content.innerText = 'Don\'t be silly. You can\'t do that.'
+        modal.style.display = "block"
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+    lookat(){
+        // default modal "Pretty cool"
+    }
+    talkTo(){
+        // default modal "You look ridiculous right now. You know that, right?"
+    }
+    grab(){
+        // default modal "Careful now."
+    }
+    drop(){
+        // default modal "Don't be silly. You can't do that."
+    }
+    use(){
+        // default modal "Don't be silly. You can't do that."
+    }
+}
+// build out all possible inventory items
+// chewing gum (in inventory at start of game)
+const gum = new Item('chewing gum', true)
+const leaf = new Item('leaf', false)
+const reservation = new Item('Jackson Party of 2', false) // (invisible?)
+const jacket = new Item('jacket', false)
+const water = new Item('bottled water', false)
+const flour = new Item('flour', false) // (invisible?)
+const lotion = new Item('sparkly lotion', false)
+const dict = new Item('French/English dictionary', false)
+// inventory array
+let inventory = [gum, leaf, reservation, jacket, water, flour, lotion, dict]
 // action display window
 let actionUpdate = document.querySelector('#action-display')
 let displayedAction = actionUpdate.children[0]
@@ -8,12 +52,27 @@ let displayedTarget = actionUpdate.children[1]
 //actions container
 const clickableWords = document.getElementsByClassName('interactive')
 // action buttons
-const walkTo = document.getElementById('walk-to')
-const lookAt = document.getElementById('look-at')
-const talkTo = document.getElementById('talk-to')
-const grab = document.getElementById('grab')
-const drop = document.getElementById('drop')
-const use = document.getElementById('use')
+const walkBtn = document.getElementById('walk-to')
+const lookBtn = document.getElementById('look-at')
+const talkBtn = document.getElementById('talk-to')
+const grabBtn = document.getElementById('grab')
+const dropBtn = document.getElementById('drop')
+const useBtn = document.getElementById('use')
+
+// function to display inventory array items in the #inventory div
+const checkInventory = () => {
+    let inventoryList = document.querySelector('ul')
+
+    for (let i=0; i < inventory.length; i++) {
+        if (inventory[i].inHand === true){
+        const item = document.createElement('li')
+        item.innerText = inventory[i].name
+        item.classList.add('interactive')
+        inventoryList.appendChild(item)
+        }
+    }
+    console.log(inventoryList)
+}
 
 // add event listeners to all interactive text that sets the clicked action to 'active'
 const generateButtons = () => {
@@ -41,5 +100,6 @@ const makeActive = (e) => {
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    generateButtons()
+    checkInventory()
+    generateButtons()  
 })
